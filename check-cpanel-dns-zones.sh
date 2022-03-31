f2978158c3feeb5d7363bebb251e83e2f98e753f
Amirhossein Matini <27870951+matiniamirhossein@users.noreply.github.com>
2022-04-01 00:39:06 +0430

    Create check-cpanel-dns-zones.sh

---
+++ check-cpanel-dns-zones.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Checks the Named Configuration then
+# Checks all zones in the named.conf
+#
+# Written by Troy Germain
+
+# Base location of named configuration file
+NAMEDCONF="/etc/named.conf"
+
+# Base Path to the Zone Files
+ZONEBASE="/var/named/"
+
+# Command Path for named- commands
+COMPATH="/usr/sbin/"
+
+#CHROOT location if applicable, if not just use null definition
+#CHROOT=""
+
+eval FILES=( $(sed -e 's/^[ \t]*//' ${CHROOT}${NAMEDCONF} | grep ^file | grep -v '^//' | awk -F\" '{printf "%s ", $(NF-1)}') )
+
+${COMPATH}named-checkconf
+if [[ $? != 0 ]]; then
+    echo "named.conf Configuration Check Failed!"
+    exit 1
+fi
+
+echo "Named Config Test Passed"
+
+# Loop starts at 1 instead of 0 because of definition for named.ca
+for (( LOOP=1; LOOP<${#FILES[*]}; LOOP=LOOP+1 )); do
+    if [[ ${FILES[${LOOP}]} = "/var/named/named.ca" ]]; then
+        continue
+    fi
+    ${COMPATH}named-checkzone $(basename ${FILES[${LOOP}]} .db) ${FILES[${LOOP}]} > /dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        echo "Check Failed! - $(basename ${FILES[${LOOP}]} .db) against ${FILES[${LOOP}]}"
+        exit 1
+    fi
+done
+
+echo "All Zone Files pass"
+
+echo "All OK - Safe to Reload!!"
